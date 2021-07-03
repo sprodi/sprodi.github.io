9fa0587a61b91c4ad6ff1accfc5efb3698eb12e6
Sprodi <seanprodi@gmail.com>
2021-07-02 18:12:07 -0700

    changed dark mode toggle and added 'plantr.html'

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,25 @@
+document.getElementById('toggle').addEventListener('click', function() {
+  let htmlClasses = document.querySelector('html').classList;
+  if(localStorage.theme == 'dark') {
+      htmlClasses.remove('dark');
+      localStorage.removeItem('theme')
+  } else {
+      htmlClasses.add('dark');
+      localStorage.theme = 'dark';
+  }
+});
+
+
+if (localStorage.theme === 'dark' || (!'theme' in localStorage && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.querySelector('html').classList.add('dark')
+} else if (localStorage.theme === 'dark') {
+  document.querySelector('html').classList.add('dark')
+}
+
+
+
+
+
 var controller = new ScrollMagic.Controller({
    globalSceneOptions: {
       triggerHook: 'onLeave',
