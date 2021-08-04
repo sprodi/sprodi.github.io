530ca1ec2b4f93be5a180a5d0cf0552a245ed24f
sProdi <seanprodi@gmail.com>
2021-08-04 12:33:01 -0700

    fixed dark mode colors and minor layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,22 @@ module.exports = {
       './**/*.js',
    ],
    theme: {
+      backgroundPosition: {
+         bottom: 'bottom',
+        'bottom-4': 'center bottom -8rem',
+         center: 'center',
+         left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+         right: 'right',
+         'right-bottom': 'right bottom',
+         'right-top': 'right top',
+         top: 'top',
+        'top-4': 'center top 1rem',
+       },
       letterSpacing: {
          widest: '.5em',
+         wider: ' 0.05em',
       },
       fontSize: {
          '2xs': '.5rem',
